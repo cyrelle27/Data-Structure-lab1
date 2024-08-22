b1caa74e44d406e51f14318c8b96be1301294711
cyril <jhoncyril.malagum@gmail.com>
2024-08-22 11:59:21 +0800

    very demure very cutesy , initial commit

---
+++ act#1.py
@@ -0,0 +1,9 @@
+firstNumber = int(input("Enter first number: "))
+secondNumber = int(input("Enter second number: "))
+
+if firstNumber < secondNumber:
+    print( "first number is less than second number")
+elif firstNumber > secondNumber:
+    print("first number is greater than second number")
+else:
+    print("first number is equals to second number")
---
+++ act#2.py
@@ -0,0 +1,5 @@
+nameOne = input("Enter class name 1: ")
+nameTwo = input("Enter class name 2: ")
+nameThree = input("Enter class name 3: ")
+
+print("Your classmates are: " + nameOne + ", " + nameTwo + " & " + nameThree )
